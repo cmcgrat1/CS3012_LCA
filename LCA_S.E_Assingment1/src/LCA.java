bec650a052d44be98e2a08f63a2a236fe1b2e6fb
cmcgrat1 <48024698+cmcgrat1@users.noreply.github.com>
2019-10-08 16:10:06 +0100

    Update LCA.java

--- LCA_S.E_Assingment1/src/LCA.java
+++ LCA_S.E_Assingment1/src/LCA.java
@@ -82,14 +82,14 @@ public class LCA
     { 
         LCA tree = new LCA(); 
         tree.root = new Node(5);
-		    tree.root.left = new Node(7);
-		    tree.root.right = new Node(6);
-		    tree.root.right.right = new Node(4);
-		    tree.root.left.right =new Node(3);
-		    tree.root.left.left =new Node(1);
-		    tree.root.right.left =new Node(0);
-		    tree.root.right.right.right =new Node(8);
-		    tree.root.right.right.left =new Node(9);
+        tree.root.left = new Node(7);
+        tree.root.right = new Node(6);
+	tree.root.right.right = new Node(4);
+	tree.root.left.right =new Node(3);
+	tree.root.left.left =new Node(1);
+	tree.root.right.left =new Node(0);
+	tree.root.right.right.right =new Node(8);
+	tree.root.right.right.left =new Node(9);
   
         System.out.println("LCA(4, 5): " + tree.findLCA(4,5)); 
         System.out.println("LCA(4, 6): " + tree.findLCA(4,6)); 
