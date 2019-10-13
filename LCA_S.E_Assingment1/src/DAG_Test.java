705864a8b93c722b95c772f91e3ae19907da514e
cmcgrat1 <48024698+cmcgrat1@users.noreply.github.com>
2019-10-13 14:50:37 +0100

    Test E and test V added

--- LCA_S.E_Assingment1/src/DAG_Test.java
+++ LCA_S.E_Assingment1/src/DAG_Test.java
@@ -4,9 +4,24 @@ import org.junit.Test;
 
 public class DAG_Test {
 
+	@Test 
+	public void testV()
+	{
+		DAG graph = new DAG(4);
+		assertEquals(4, graph.V());
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testE(){
+		
+		DAG graph = new DAG(8);
+		
+		graph.addEdge(1, 5);
+		graph.addEdge(5, 4);
+		graph.addEdge(4, 3);
+		graph.addEdge(3, 5);
+		
+		assertEquals(4, graph.E());
 	}
 
 }
