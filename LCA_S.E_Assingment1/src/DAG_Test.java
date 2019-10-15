8a9cb118afec711da0c491fedd4cb4ac16994ec9
cmcgrat1 <48024698+cmcgrat1@users.noreply.github.com>
2019-10-15 11:20:44 +0100

    Added Test DAG and exception test

--- LCA_S.E_Assingment1/src/DAG_Test.java
+++ LCA_S.E_Assingment1/src/DAG_Test.java
@@ -3,6 +3,33 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class DAG_Test {
+	
+	@Test
+	public void testDAG()
+	{
+		DAG graph = new DAG(12);
+		
+		graph.addEdge(1, 2);
+		graph.addEdge(2, 4);
+		graph.addEdge(2, 5);
+		graph.addEdge(4, 6);
+		graph.addEdge(4, 7);
+		
+		assertEquals(1, graph.indegree(4));
+		assertEquals(2, graph.outdegree(4));
+		assertEquals(5, graph.E());
+		assertEquals(12, graph.V());
+		String adj = "[6, 7]";
+		assertEquals(adj, graph.adj(4).toString());
+	}
+	
+	@Test(expected=Exception.class)
+	public void exceptionTest(){
+		
+		//Can't make a directed graph with less than 0 vertices
+		
+		DAG graph = new DAG(-5);
+	}
 
 	@Test 
 	public void testV()
