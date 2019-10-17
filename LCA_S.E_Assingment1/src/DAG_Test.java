61d7e3f1ee35e84091e7520f8a8198ac529c5a17
cmcgrat1 <48024698+cmcgrat1@users.noreply.github.com>
2019-10-17 11:33:20 +0100

    testing for cycle added

--- LCA_S.E_Assingment1/src/DAG_Test.java
+++ LCA_S.E_Assingment1/src/DAG_Test.java
@@ -93,6 +93,22 @@ public class DAG_Test {
 		assertEquals(1, graph.E());
 	}
 	
+	@Test
+	public void testforCycle()
+	{
+		DAG graph = new DAG(6);
+		
+		graph.addEdge(0, 1);
+		graph.addEdge(1, 2);
+		graph.addEdge(2, 3);
+		graph.addEdge(3, 0);
+		graph.addEdge(3, 4);
+		graph.addEdge(4, 5);
+		
+		graph.findCycle(0);
+		
+		assertTrue(graph.hasCycle());
+	}
 
 
 }
