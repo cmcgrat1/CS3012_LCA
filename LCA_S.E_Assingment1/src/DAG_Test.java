1f6bfe26c1683e3394b1d21f113d019d247a118e
cmcgrat1 <48024698+cmcgrat1@users.noreply.github.com>
2019-10-18 14:33:17 +0100

    added LCA test

--- LCA_S.E_Assingment1/src/DAG_Test.java
+++ LCA_S.E_Assingment1/src/DAG_Test.java
@@ -122,7 +122,26 @@ public class DAG_Test {
 		graph.findCycle(1);
 		assertFalse(graph.hasCycle());
 	}
-	
+	@Test
+	public void testLCA()
+	{
+		DAG graph = new DAG(10);
+		
+		graph.addEdge(0, 1);
+		graph.addEdge(1, 3);
+		graph.addEdge(1, 2);
+		graph.addEdge(3, 4);
+		graph.addEdge(2, 5);
+		graph.addEdge(5, 6);
+		graph.addEdge(4, 6);
+		graph.addEdge(6, 7);
+		graph.addEdge(4, 8);
+		graph.addEdge(8, 7);
+		
+		assertEquals(1, graph.findLCA(4, 5));
+		assertEquals(8, graph.findLCA(7, 8));
+		assertEquals(6, graph.findLCA(6, 7));
+	}
 
 
 }
